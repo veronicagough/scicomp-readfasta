b8db3ff3488f8c7a4e559d193869f4cb0460ba4a
Veronica Gough <veronica.gough@duke.edu>
2018-10-31 09:16:02 -0400

    Initial version of fasta reader

---
+++ read_fasta.py
@@ -0,0 +1,11 @@
+def read_fasta(filename):
+    sequence = ''
+    f = open(filename)
+    for line in f:
+        line = line.strip()
+        if not '>' in line:
+            sequence = sequence + line
+    f.close()
+    return sequence
+
+print(read_fasta('ae.fa'))
