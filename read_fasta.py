23fa204fe722fa9f440d34bd7ae7356ce9620d0f
Veronica Gough <veronica.gough@duke.edu>
2018-10-31 09:28:58 -0400

    Changes hardcoded file name to command line argument

--- read_fasta.py
+++ read_fasta.py
@@ -1,3 +1,5 @@
+import sys
+
 def read_fasta(filename):
     sequence = ''
     f = open(filename)
@@ -8,4 +10,4 @@ def read_fasta(filename):
     f.close()
     return sequence
 
-print(read_fasta('ae.fa'))
+print(read_fasta(sys.argv[1]))
